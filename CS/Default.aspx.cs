aa82761c241322d76c47630777bbd2ccfed278e3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿#region #usings
+using DevExpress.Web;
+#endregion #usings
+
+public partial class _Default : System.Web.UI.Page {
+    #region #addingItems
+    const int itemsCount = 10;
+    protected void DataView_Load(object sender, System.EventArgs e) {
+        AddDataItems(DataView);
+        DataView.ItemTemplate = new MyDataViewTemplate();
+    }
+    void AddDataItems(ASPxDataView dataView) {
+        for (int i = 1; i <= itemsCount; i++) {
+            dataView.Items.Add().DataItem = new {
+                ID = i,
+                Name = "Name" + i,
+                Description = "Sample description for the item"
+            };
+        }
+    }
+    #endregion #addingItems
+}
---
+++ VB/App_Code/MyDataViewTemplate.vb
@@ -0,0 +1,25 @@
+﻿Imports DevExpress.Web
+Imports System.Web.UI
+
+Public Class MyDataViewTemplate
+    Implements ITemplate
+
+    Public Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
+        Dim dataViewContainer = TryCast(container, DataViewItemTemplateContainer)
+        container.Controls.Add(GetFormLayout(dataViewContainer.DataItem))
+    End Sub
+    Private Function GetFormLayout(ByVal dataItem As Object) As ASPxFormLayout
+        Dim formLayout = New ASPxFormLayout()
+        Dim layoutGroup = TryCast(formLayout.Items.Add(New LayoutGroup("Item Info")), LayoutGroup)
+        Dim layoutItemName = New LayoutItem("Name")
+        layoutItemName.Controls.Add(New ASPxLabel() With {.Text = GetData(dataItem, "Name")})
+        Dim layoutItemDecsription = New LayoutItem("Description")
+        layoutItemDecsription.Controls.Add(New ASPxLabel() With {.Text = GetData(dataItem, "Description")})
+        layoutGroup.Items.Add(layoutItemName)
+        layoutGroup.Items.Add(layoutItemDecsription)
+        Return formLayout
+    End Function
+    Private Function GetData(ByVal dataObject As Object, ByVal fieldName As String) As String
+        Return DataBinder.Eval(dataObject, fieldName).ToString()
+    End Function
+End Class
