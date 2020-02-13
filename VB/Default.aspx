9d95b6903d25e4ba5aee3dd853934417b6f060b6
DevExpressExampleBot <fakeEmail>
2020-02-13 10:27:50 +0300

    autogenerate VB

--- VB/Default.aspx
+++ VB/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="vb" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
 <!DOCTYPE html>
 
--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -3,18 +3,22 @@ Imports DevExpress.Web
 #End Region ' #usings
 
 Partial Public Class _Default
-    Inherits System.Web.UI.Page
+	Inherits System.Web.UI.Page
 
-    #Region "#addingItems"
-    Private Const itemsCount As Integer = 10
-    Protected Sub DataView_Load(ByVal sender As Object, ByVal e As System.EventArgs)
-        AddDataItems(DataView)
-        DataView.ItemTemplate = New MyDataViewTemplate()
-    End Sub
-    Private Sub AddDataItems(ByVal dataView As ASPxDataView)
-        For i As Integer = 1 To itemsCount
-            dataView.Items.Add().DataItem = New With {Key .ID = i, Key .Name = "Name" & i, Key .Description = "Sample description for the item"}
-        Next i
-    End Sub
-    #End Region ' #addingItems
+	#Region "#addingItems"
+	Private Const itemsCount As Integer = 10
+	Protected Sub DataView_Load(ByVal sender As Object, ByVal e As System.EventArgs)
+		AddDataItems(DataView)
+		DataView.ItemTemplate = New MyDataViewTemplate()
+	End Sub
+	Private Sub AddDataItems(ByVal dataView As ASPxDataView)
+		For i As Integer = 1 To itemsCount
+			dataView.Items.Add().DataItem = New With {
+				Key .ID = i,
+				Key .Name = "Name" & i,
+				Key .Description = "Sample description for the item"
+			}
+		Next i
+	End Sub
+	#End Region ' #addingItems
 End Class
